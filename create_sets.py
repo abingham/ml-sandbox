5ba6e0df3b017d1f39f3504aad725acb52ac589d
Austin Bingham <austin.bingham@gmail.com>
2019-06-05 09:42:31 +0200

    Refactored a bit.

--- create_sets.py
+++ create_sets.py
@@ -1,46 +1,55 @@
+"""Copy the VOC2012 images into the right place.
+"""
+
 from itertools import islice
 from pathlib import Path
 import shutil
+import sys
 
 
-IMAGE_INPUT_DIR = Path('../data/VOCdevkit/VOC2012') / 'JPEGImages'
-MASK_INPUT_DIR = Path('../data/VOCdevkit/VOC2012') / 'SegmentationClass'
+class Copier:
+    def __init__(self, voc_path, data_root='data'):
+        self._voc_path = Path(voc_path)
+        self._data_root = Path(data_root)
 
-DATA_ROOT = Path('data')
-TRAINING_IMAGE_DIR = DATA_ROOT / 'training_images'
-TRAINING_MASK_DIR = DATA_ROOT / 'training_masks'
-VALIDATION_IMAGE_DIR = DATA_ROOT / 'validation_images'
-VALIDATION_MASK_DIR = DATA_ROOT / 'validation_masks'
-TESTING_IMAGE_DIR = DATA_ROOT / 'test_images'
-TESTING_MASK_DIR = DATA_ROOT / 'test_masks'
+        self.IMAGE_INPUT_DIR = self._voc_path / 'JPEGImages'
+        self.MASK_INPUT_DIR = self._voc_path / 'SegmentationClass'
+
+        self.TRAINING_IMAGE_DIR = self._data_root / 'training_images'
+        self.TRAINING_MASK_DIR = self._data_root / 'training_masks'
+        self.VALIDATION_IMAGE_DIR = self._data_root / 'validation_images'
+        self.VALIDATION_MASK_DIR = self._data_root / 'validation_masks'
+        self.TESTING_IMAGE_DIR = self._data_root / 'test_images'
+        self.TESTING_MASK_DIR = self._data_root / 'test_masks'
+
+    def num_images(self):
+        return len(tuple(self.MASK_INPUT_DIR.iterdir()))
+
+    def input_data(self):
+        for mask in self.MASK_INPUT_DIR.iterdir():
+            yield (self.IMAGE_INPUT_DIR / f'{mask.stem}.jpg', mask)
+
+    def copy(self):
+        training_split = int(0.7 * self.num_images())
+        validation_split = int(0.9 * self.num_images())
+
+        data_sets = {
+            (self.TRAINING_IMAGE_DIR, self.TRAINING_MASK_DIR): islice(self.input_data(), training_split),
+            (self.VALIDATION_IMAGE_DIR, self.VALIDATION_MASK_DIR): islice(self.input_data(), training_split, validation_split),
+            (self.TESTING_IMAGE_DIR, self.TESTING_MASK_DIR): islice(self.input_data(), validation_split, self.num_images()),
+        }
+        for (img_dir, mask_dir), inputs in data_sets.items():
+            img_dir.mkdir(parents=True)
+            mask_dir.mkdir(parents=True)
+            for img, mask in inputs:
+                shutil.copy(img, img_dir)
+                shutil.copy(mask, mask_dir)
 
 
-def num_images():
-    return len(tuple(MASK_INPUT_DIR.iterdir()))
-
-
-def input_data():
-    for mask in MASK_INPUT_DIR.iterdir():
-        yield (IMAGE_INPUT_DIR / f'{mask.stem}.jpg', mask)
-
-
-def main():
-    training_split = int(0.7 * num_images())
-    validation_split = int(0.9 * num_images())
-
-    data_sets = {
-        (TRAINING_IMAGE_DIR, TRAINING_MASK_DIR): islice(input_data(), training_split),
-        (VALIDATION_IMAGE_DIR, VALIDATION_MASK_DIR): islice(input_data(), training_split, validation_split),
-        (TESTING_IMAGE_DIR, TESTING_MASK_DIR): islice(input_data(), validation_split, num_images()),
-    }
-    for (img_dir, mask_dir), inputs in data_sets.items():
-        img_dir.mkdir(parents=True)
-        mask_dir.mkdir(parents=True)
-        for img, mask in inputs:
-            shutil.copy(img, img_dir)
-            shutil.copy(mask, mask_dir)
+def main(args):
+    c = Copier(args[1])
+    c.copy()
 
 
 if __name__ == '__main__':
-    main()
-
+    main(sys.argv)
